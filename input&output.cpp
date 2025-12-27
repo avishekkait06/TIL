af59021b05779a89dce25d97bb4be16991ba8b7e
Avishek Kait <avishekkait@gmail.com>
2025-12-27 23:12:35 +0600

    FIRST_INPUT_OUTPUT__BY__C++

---
+++ input&output.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}   
+
+
+int main() {
+    fastIO();
+    
+    int a;
+    cin >> a;
+    cout << a << "\n";
+
+    return 0;
+}       
