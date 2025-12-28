a63469be0abd1c6a5848e8e67afba688298c1653
Avishek Kait <avishekkait@gmail.com>
2025-12-28 16:58:10 +0600

    LCD

---
+++ LCD.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b){
+    int rem;
+    if(b==0) return a;
+    else{
+        while(b!=0){
+        rem=a%b;
+        a=b;
+        b=rem;
+        
+    }
+    return a;
+}}
+int lcd(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+int main(){
+int n1,n2;
+cout<<"Enter Two Numbers:"<<endl;
+cin>>n1>>n2;
+cout <<"LCD is: ";
+cout << lcd(n1,n2)<<endl;
+return 0;
+}
