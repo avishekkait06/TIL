41bbe295cfc7640071b1efe178a2d959c03610f6
Avishek Kait <avishekkait@gmail.com>
2025-12-30 23:58:45 +0600

    ASCII Value

---
+++ ASCII_Value.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;        
+int main(){
+    char c;
+    scanf("%c",&c);          // Using scanf() to read a character
+    printf("%d\n",c);   // Using printf() to print the ASCII value
+
+    return 0;
+}
