63c84648e91fcda881f9b32981cff131d1fd7c48
Avishek Kait <avishekkait@gmail.com>
2025-12-31 17:11:55 +0600

    Array Sum

---
+++ Array Sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    int a[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){               
+        scanf("%d",&a[i]);
+        sum+=a[i];
+    }   
+    printf("%d",sum);
+    return 0;
+}
