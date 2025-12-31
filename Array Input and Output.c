c6b4c79557ad79348794dc2d6ff51a5ddacbc16d
Avishek Kait <avishekkait@gmail.com>
2025-12-31 17:13:24 +0600

    Array Input and Output

---
+++ Array Input and Output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3];
+
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
