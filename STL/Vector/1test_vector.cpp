bfacefaf64ee55d306710e696df3d6bd7f310d55
Avishek Kait <avishekkait@gmail.com>
2026-01-04 23:17:38 +0600

    test 1

---
+++ STL/Vector/1test_vector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    vector<int>v(5); // Vector of size 5
+// all element are 0 initially
+    for(int i = 0; i < 5; i++ ) 
+    {
+        cout << v[i] << " "; // Output the ith element
+        
+    }
+    return 0;
+}
