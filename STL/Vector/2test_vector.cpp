fc22752023a2f248c29679ee47b3cc655ab1de4e
Avishek Kait <avishekkait@gmail.com>
2026-01-04 23:18:58 +0600

    test 2

---
+++ STL/Vector/2test_vector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    vector<int>v(10 , 5); // Vector of size 10
+// all element are 5 initially
+    for(int i = 0; i < 10; i++ ) 
+    {
+        cout << v[i] << " "; // Output the ith element
+        
+    }
+    return 0;
+}
