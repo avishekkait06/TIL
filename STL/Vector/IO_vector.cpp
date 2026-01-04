0e38cf5f180b92bfc188954e9a08e0fcab7f3e2f
Avishek Kait <avishekkait@gmail.com>
2026-01-04 21:38:13 +0600

    Input and Output Vector element

---
+++ STL/Vector/IO_vector.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    vector<int>v;
+    
+    v.push_back(20); 
+    v.push_back(30);
+    v.push_back(40);    
+    v.push_back(50); // Insert 50 at the end
+
+
+  cout<< v[0] <<endl; // Output the 1st element (index 0)
+  cout<< v[1] <<endl; // Output the 2nd element (index 1)
+  cout<< v[2] <<endl; // Output the 3rd element (index 2)
+  cout<< v[3] <<endl; // Output the 4th element (index 3) 
+  
+
+
+    return 0;
+}
