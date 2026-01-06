3cbb02eba028e0de1106bc889f82f4aefc1d4622
Avishek Kait <avishekkait@gmail.com>
2026-01-06 23:57:29 +0600

    Iterator

---
+++ STL/Vector/iterator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    vector<int>::iterator it;
+
+    for(it = v.begin(); it != v.end(); it++) {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
