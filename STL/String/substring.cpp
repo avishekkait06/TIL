16b2a0ffeba8d82e860f58e0bc50ac44ecd1f297
Avishek Kait <avishekkait@gmail.com>
2026-01-14 12:57:00 +0600

    substring

--- STL/String/substring.cpp
+++ STL/String/substring.cpp
@@ -1,7 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     string s = "hello ";
-    cout << s.substr(0,3) << endl;
+    cout << s.substr(0, 1) << endl;
+    cout << s.substr(0, 2) << endl;
+    cout << s.substr(0, 3) << endl;
+    cout << s.substr(0, 4) << endl;
+    cout << s.substr(0, 5) << endl;
     return 0;
 }
