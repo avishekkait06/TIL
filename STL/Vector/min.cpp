3f8dc463a79aa4778b1b5671a4634c11e8224dca
Avishek Kait <avishekkait@gmail.com>
2026-01-21 17:48:43 +0600

    io pair

---
+++ STL/Vector/min.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for( int *it ; x){
+        cin >> x;
+    }
+    int m= *max_element(arr,arr+5);
+    cout << m << endl;
+    return 0;
+}
---
+++ STL/pair/iopair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    pair<int,int>c,c1;
+    cin >> c.first >> c.second;
+      cin >> c1.first >> c1.second;
+      cout << "output =" << endl;
+      cout << c.first  <<" "<< c.second<< endl;
+      cout << c1.first  <<" "<< c1.second;
+      return 0;
+}
