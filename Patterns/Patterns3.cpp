6d12075d7eaefccfb16d758e6b1bd464cbe79396
Avishek Kait <avishekkait@gmail.com>
2026-02-08 23:42:41 +0600

    Patterns

---
+++ Patterns/Patterns3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=4;
+    for(int i =1;i<=4;i++){
+        for(int j=0;j<i;j++){
+            cout<<"* ";
+        }
+        cout <<endl;
+    }
+    return 0;
+}
