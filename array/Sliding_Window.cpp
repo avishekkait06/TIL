89fb2b45c5db598ddf487055b124475924ab545c
Avishek Kait <avishekkait@gmail.com>
2026-04-01 16:01:42 +0600

    Sliding Window

---
+++ array/Sliding_Window.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n , w ,i =0 ;
+    cout << "enter the number total nuber of a  array :"<<endl;
+    cin >> n ;
+    vector <int> array(n);
+    for(int i = 0 ; i <n ; i++){
+        cin >> array[i];
+    }
+}
+
+int main() {
+    
+    
+        solve();
+    
+    
+    return 0;
+}
