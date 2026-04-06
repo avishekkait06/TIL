9c7fb35a320e73e0bc68a11b2c5d7a4042e866a0
Avishek Kait <avishekkait@gmail.com>
2026-04-06 18:40:30 +0600

    i[arr]

---
+++ array/parary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+    
+    int n ;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0 ; i < n ; i++){
+        scanf( "%d", &a[i]);
+    }
+printf("%d",2[a]);//In C and C++, a[i] and i[a] print the exact same value.
+    return 0;
+}
