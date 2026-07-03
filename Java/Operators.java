a46cdb13632a1b9740eee574f977ef9520c46425
Avishek Kait <avishekkait@gmail.com>
2026-07-03 22:00:05 +0600

    Java Operators

---
+++ Java/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] agrs) {
+        int a = 10;
+        int b = 6;
+        System.out.println("Add :" + (a + b));
+        System.out.println("Subtraction :" + (a - b));
+        System.out.println("Multipication :" + (a + b));
+        System.out.println("divsion : " + (a + b));
+        System.out.println("pre increment : " + ++a);
+        System.out.println("Post increment :" + a++);
+        System.out.println("Now :" + a);
+
+    }
+}
